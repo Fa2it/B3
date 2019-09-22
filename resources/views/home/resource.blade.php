fc7fece628e4d3dd36e852d391f744078ba19212
Fa2it <ashu_felix@yahoo.co.uk>
2019-09-22 14:11:24 +0200

    ffff

--- resources/views/home/resource.blade.php
+++ resources/views/home/resource.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row justify-content-center">
         <div class="col-md-10">
-          <h1>Resources {{ $resource }}</h1>
+          <h1>Resources {{ $resource }}</h1> 
         </div>
     </div>
 @endsection
