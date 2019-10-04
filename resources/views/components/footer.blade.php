22becf64041bbbff9224da6b284edc741823d4ba
Fa2it <ashu_felix@yahoo.co.uk>
2019-10-05 00:26:20 +0200

    added footer and styles

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,37 @@
+<footer class="container-fluid">
+  <div class="row bg-secondary">
+      <div class="container pt-4 pb-4 text-light">
+        <div class="row">
+              <div class="col">
+                  <h3>About Us</h3>
+                  <p> bla bla bla bla </p>
+              </div>
+              <div class="col">
+                  <h3>Get Intouch</h3>
+                  <p> bla bla bla bla </p>
+              </div>
+              <div class="col">
+                  <h3>Site Map</h3>
+                  <p> bla bla bla bla </p>
+              </div>
+        </div>
+      </div>
+  </div><!-- Pick Plan Section Ends -->
+  <div></div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
+<script>
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#237afc"
+    },
+    "button": {
+      "background": "#fff",
+      "text": "#237afc"
+    }
+  },
+  "theme": "edgeless"
+});
+
+</script>
--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -2,9 +2,50 @@
 
 @section('content')
 <div class="row bg-primary">
-    <div class="container justify-content-center">
+    <div class="container">
+      <div class="w-75 mx-auto">
       <h2 class="mt-3 text-white">Increase your success rate and gain market understandings</h2>
             @include('components.searchform')
+      </div>
     </div>
+</div>
+<div class="row mt-5">
+  <div class="container">
+    <div class="row">
+        <div class="col bg-primary">
+          <div class="card" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+              </div>
+          </div>
+        </div>
+        <div class="col bg-primary">
+          <div class="card" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+              </div>
+          </div>
+        </div>
+        <div class="col bg-primary">
+          <div class="card" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+              </div>
+          </div>
+        </div>
+    </div>
+  </div>
 </div>
 @endsection
